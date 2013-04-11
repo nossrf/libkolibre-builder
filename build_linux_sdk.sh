bcaa1b20da1aeb2cc38675cc092b9d15d150abce
Johan Abbors <johan@pratsam.com>
2013-04-11 16:09:46 +0300

    Add license header to linux build script

--- build_linux_sdk.sh
+++ build_linux_sdk.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+## Copyright (C) 2012 Kolibre
+#
+# This file is part of kolibre-builder.
+#
+# Kolibre-builder is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# Kolibre-builder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with kolibre-builder. If not, see <http://www.gnu.org/licenses/>.
+#
+
 set -e
 set -x
 
